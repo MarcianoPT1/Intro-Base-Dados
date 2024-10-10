337475a2f63773c7b27062598a8bba4b06554206
Márcio Canuto <marciocanuto05@gmail.com>
2024-10-10 18:40:08 +0100

    Create primeiro.py

---
+++ primeiro.py
@@ -0,0 +1,7 @@
+name = input("Qual é o teu nome? ")
+age = input("Quantos anos tens?")
+consulta = input("Qual a especialidade da consulta? ")
+data = input("Quando vai ser a consulta? ")
+
+txt = f'O paciente {name} que têm {age} anos e vai ter consulta de {consulta} no dia {data}.'
+print(txt)
